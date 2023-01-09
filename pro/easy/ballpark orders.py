a0129ed04ff17ec7cf975a845805e899b6de84ed
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-01-09 20:42:32 +0100

    ballpark orders simplified

--- pro/easy/ballpark orders.py
+++ pro/easy/ballpark orders.py
@@ -1,11 +1,8 @@
-order = input()
-
+order = input().split(" ")
 menu = {"Nachios": 6, "Pizza": 6, "Cheeseburger": 10, "Water": 4, "Coke": 5}
-
 sum = 0
-new_order = order.split(" ")
 
-for item in new_order:
+for item in order:
     if item in menu:
         sum += menu[item]
     else:
