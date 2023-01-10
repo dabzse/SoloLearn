50d44d42dd6f1815e95928388faf4c012e53c78f
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-01-10 12:39:36 +0100

    add: easter eggs in python

---
+++ pro/easy/easter eggs.py
@@ -0,0 +1,8 @@
+easter_eggs = int(input())
+my_basket = int(input())
+friends_basket = int(input())
+
+if my_basket + friends_basket < easter_eggs:
+    print("Keep Hunting")
+else:
+    print("Candy Time")
