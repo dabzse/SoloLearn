1479714f04ba5cab748b399cdf1b3014dac5bce1
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-04-24 22:14:28 +0200

    add: code-coach: fruit_bowl.cpp

---
+++ code-coach/free/easy/fruit_bowl.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fruit;
+
+    // take input
+    cin >> fruit;
+
+    // your code goes here
+    int apples = fruit / 2;
+    int cakes = apples / 3;
+    cout << cakes << endl;
+
+    return 0;
+}
