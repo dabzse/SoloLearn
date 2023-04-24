63489bb88525abc1cef2a4cb407a98bb14140c6c
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-04-24 19:36:04 +0200

    add: adding data

---
+++ courses/sql-intermediate/adding_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (firstname, lastname, salary) VALUES
+    ('Wang', 'Lee', 1900),
+    ('Greta', 'Wu', 1200);
+SELECT id, firstname, lastname, salary
+FROM Employees
+ORDER BY id DESC;
