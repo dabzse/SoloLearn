0085377bcb9b0762eb4e82dcf26d7f1fb2b44eeb
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-04-29 21:03:49 +0200

    add: player destructor

---
+++ courses/cpp-intermediate/player_destructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Player {
+    public:
+        int points;
+        Player(int x) {
+            points = x;
+            points %= 5;
+        }
+        // define the destructor
+        ~Player() {
+            cout << points << endl;
+        }
+};
+
+int main() {
+    int points;
+    cin >> points;
+    Player obj(points);
+}
