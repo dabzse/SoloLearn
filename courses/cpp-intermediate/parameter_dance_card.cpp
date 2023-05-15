645f31e2b7529a58af7853c4ba6d72359e576594
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-05-15 20:08:37 +0200

    add: parameter dance card

---
+++ courses/cpp-intermediate/parameter_dance_card.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// change the function
+template <class T, class U>
+T myMax (T a, U b) {
+    return (a > b ? a : b);
+}
+
+int main() {
+    double x;
+    int y;
+    cin >> x >> y;
+    cout myMax(x, y) << endl;
+
+    return 0;
+}
