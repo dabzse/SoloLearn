3e24a0e9fd33034880c3edbc6ce7af3bb40e0808
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-05-27 17:06:30 +0200

    add: card numbers

---
+++ courses/csharp-intermediate/card_numbers.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SoloLearn {
+    class Program {
+        static void Main(string[] args) {
+            Card card1 = new Card();
+
+            // fix the output
+            Console.WriteLine(card1.AccountNum);
+        }
+    }
+    class Card {
+        private string accountNum = "0011592048120";
+        // create a property to get the account
+        public string AccountNum {
+            get { return accountNum; }
+        }
+    }
+}
