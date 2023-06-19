c7b45a10f8898ce6811774590d5288e7a75fc3ef
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-06-19 20:02:05 +0200

    add: robot-barmen (or: robot-barman)

---
+++ courses/csharp-intermediate/robot-barmen.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+namespace SoloLearn {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                // your code goes here
+                Console.WriteLine(drinks / shelves);
+            }
+
+            /**
+            * 1. DivideByZeroException => "At least 1 shelf"
+            * 2. FormatException => "Please insert an integer"
+            */
+            catch (DivideByZeroException) {
+                Console.WriteLine("At least 1 shelf");
+            }
+            catch (FormatException) {
+                Console.WriteLine("Please insert an integer");
+            }
+        }
+    }
+}
