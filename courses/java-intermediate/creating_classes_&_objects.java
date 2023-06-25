2085a87a73ca7ea0f88e363ab4b5d12fd1c9b203
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-06-25 10:24:33 +0200

    add: new course: java intermediate: creating classes & objects

---
+++ courses/java-intermediate/creating_classes_&_objects.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // creating a Loading object with the same name
+        Loading loading = new Loading();
+        loading.LoadingMessage();
+    }
+}
+
+class Loading {
+    //mcomplete the class, add LoadingMessage() method
+    public void LoadingMessage() {
+        System.out.println("Loading");
+    }
+}
