98c11145291545e21157891ae5b15e4cad2cecb8
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-04 22:20:47 +0200

    fix: "public class Main {}" errors, see comments

--- courses/java-intermediate/inheritance.java
+++ courses/java-intermediate/inheritance.java
@@ -19,8 +19,12 @@ class Pro extends Standard {
     }
 }
 
-public class Main {
-/** The public type Main must be defined in its own file: Java(16777541) */
+class Main {
+/** The public type Main must be defined in its own file: Java(16777541)
+ * it was:
+ * public class Main {}
+ * so I just removed the `public` and now shows no errors
+*/
     public static void main(String[] args) {
         Standard standard1 = new Standard();
         Pro pro1 = new Pro();
