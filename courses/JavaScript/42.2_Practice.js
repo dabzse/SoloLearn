36b8c7e0cdaf2c03642f8bcb1d0b31799293a969
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 13:57:30 +0200

    add: javascript: The Date Object

---
+++ courses/JavaScript/42.2_Practice.js
@@ -0,0 +1,17 @@
+// The Date Object
+
+function main() {
+    var year = parseInt(readLine(), 10);
+    var month = parseInt(readLine(), 10);
+    var day = parseInt(readLine(), 10);
+
+    console.log(getWeekDay(year, month, day));
+}
+
+function getWeekDay(year, month, day) {
+    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var date = new Date(year, month, day);
+    // complete the function
+    var day = date.getDay()
+    return names[day]
+}
