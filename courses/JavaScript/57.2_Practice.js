299aa4670b11b276d7d8ae29fd54d44a217f8cdb
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 14:10:33 +0200

    add: javascript: Loops in ECMAScript 6

---
+++ courses/JavaScript/57.2_Practice.js
@@ -0,0 +1,11 @@
+// Loops in ECMAScript 6
+
+let scores = [68, 95, 54, 84, 77, 75, 63, 74, 69, 80, 71, 63]
+
+// your code goes here
+var count = 0;
+for (let s of scores)
+    if (s >= 70)
+        count++
+
+console.log(count)
