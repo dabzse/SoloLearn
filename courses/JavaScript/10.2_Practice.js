e05a3320c8b3919c37d15b6a3ae6807e5a3607a1
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 12:51:49 +0200

    add: javascript: Logical or boolean operators 2

---
+++ courses/JavaScript/10.2_Practice.js
@@ -0,0 +1,8 @@
+// Logical or boolean operators 2
+
+function main() {
+    var hour = parseInt(readLine(), 10);
+    // Your code goes here
+    time = hour >= 0 && hour <= 12 ? "am" : "pm";
+    console.log(time)
+}
