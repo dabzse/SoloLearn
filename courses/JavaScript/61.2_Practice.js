579f86aff3322d3a082880b4e50f777aae99abb1
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 14:22:10 +0200

    add: javascript: ES6 Classes

---
+++ courses/JavaScript/61.2_Practice.js
@@ -0,0 +1,16 @@
+// ES6 Classes
+
+function main() {
+    var exam1 = parseInt(readLine(), 10);
+    var exam2 = parseInt(readLine(), 10);
+    var exam3 = parseInt(readLine(), 10);
+
+    console.log(Exams.average(exam1, exam2, exam3));
+
+}
+class Exams {
+    // your code goes here
+    static average(e1, e2, e3) {
+        return Math.round((e1 + e2 + e3) / 3);
+    }
+}
