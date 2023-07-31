de652c1b8a1c852bea322684f310d5ff5bc0f325
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 14:06:53 +0200

    add: javascript: ES6 Variables and Strings

---
+++ courses/JavaScript/56.2_Practice.js
@@ -0,0 +1,15 @@
+// ES6 Variables and Strings
+
+function main() {
+    var country = readLine();
+    var capital = readLine();
+
+    console.log(countryCard(country, capital));
+}
+
+function countryCard(country, capital){
+    // complete the function
+    // use backtick (` `) for template literal
+    const card = `Name: ${country}, Capital: ${capital}`
+    return card
+}
