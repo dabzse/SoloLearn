726c3358f7ee3e4d5e2070951a906cfb95195951
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-07-31 13:28:48 +0200

    add: javascript: Currency Converter

---
+++ courses/JavaScript/30_Code_project.js
@@ -0,0 +1,12 @@
+// Currency Converter
+
+function main() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+
+    function convert(amount, rate) {
+        return amount * rate
+    }
+
+    console.log(convert(amount, rate));
+}
