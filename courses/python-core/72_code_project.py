73e94d51ca19f1905d98e095e0821516aa8f749c
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 11:53:22 +0200

    add: python core: Fibonacci

---
+++ courses/python-core/72_code_project.py
@@ -0,0 +1,12 @@
+## Fibonacci
+num = int(input())
+
+def fibonacci(n):
+	# complete the recursive function
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci (n-2)
+
+for number in range(num):
+    print(fibonacci(number))
