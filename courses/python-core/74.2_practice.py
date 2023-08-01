8345ead812ce1403039354352eb37ceda09e8e75
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 11:59:52 +0200

    add: python core: Fun with Classes

---
+++ courses/python-core/74.2_practice.py
@@ -0,0 +1,12 @@
+## Inheritance
+class Vehicle:
+    def horn(self):
+        print("Beep!")
+
+class Car(Vehicle):
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+obj = Car("BMW", "red")
+obj.horn()
