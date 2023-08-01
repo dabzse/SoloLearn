d46596451849a3a17172879bde7e9a73bf1e0665
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 12:03:16 +0200

    add: python core: Making a Deposit

---
+++ courses/python-core/77.2_practice.py
@@ -0,0 +1,15 @@
+## Data Hiding
+class BankAccount:
+    def __init__(self, balance):
+        self._balance = balance
+
+    def __repr__(self):
+        return "Account Balance: {}".format(self._balance)
+
+    def deposit(self, amount):
+        # your code goes here
+        self._balance += amount
+
+acc = BankAccount(0)
+acc.deposit(int(input()))
+print(acc)
