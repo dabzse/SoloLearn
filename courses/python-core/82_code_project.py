9fa93661cc973de8a831f3b6b2b4d294252feb5e
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 12:07:10 +0200

    add: python core: Juice Maker

---
+++ courses/python-core/82_code_project.py
@@ -0,0 +1,17 @@
+## Juice Maker
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+
+    def __add__(self, liquid):
+        return Juice(self.name + "&" + liquid.name, self.capacity + liquid.capacity)
+
+a = Juice('Orange', 1.5)
+b = Juice('Apple', 2.0)
+
+result = a + b
+print(result)
