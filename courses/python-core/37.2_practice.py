c2b373252eb61a7f27860fb7e762887a328ddb43
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 11:14:04 +0200

    add: python core: No Dice? No Problem

---
+++ courses/python-core/37.2_practice.py
@@ -0,0 +1,11 @@
+## Modules Part1
+import random
+random.seed(int(input())) # please don't touch this lane
+        ## I think that should be "LINE" not "LANE"
+
+# generate the random values for every dice
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+
+print(dice1)
+print(dice2)
