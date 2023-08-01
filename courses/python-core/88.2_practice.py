c8a9b4c91a9ea10caa41ba71cdfb381880e5d252
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 12:15:05 +0200

    add: python core: Social Media Pro

---
+++ courses/python-core/88.2_practice.py
@@ -0,0 +1,10 @@
+## Special Sequences
+import re
+text = input()
+
+# your code goes here
+# use re.findall() with r"#\w+" as the regex
+
+match = re.findall(r"#\w+", text)
+if match:
+    print("\n".join(match))
