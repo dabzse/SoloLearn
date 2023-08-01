0d1d5f32b830e903f30439e82d906ded876201ec
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 11:31:04 +0200

    add: python core: Where's The Book?

---
+++ courses/python-core/54.2_practice.py
@@ -0,0 +1,14 @@
+## Dictionary Functions 2
+books = {
+    "Life of Pi": "Adventure Fiction",
+    "The Three Musketeers": "Historical Adventure",
+    "Watchmen": "Comics",
+    "Bird Box": "Horror",
+    "Harry Potter":"Fantasy Fiction",
+    "Good Omens": "Comedy"
+}
+
+book = input()
+
+# change this part to use the .get() method
+print(books.get(book, "Book not found"))
