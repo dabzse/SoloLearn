385a0c6773931a01b9c57476430be84aff95ac3d
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-01 11:18:56 +0200

    add: python core: Bank card PIN System

---
+++ courses/python-core/42.2_practice.py
@@ -0,0 +1,11 @@
+## Exception Handling
+pin = input()
+
+try:
+	# your code goes here
+    pin = int(pin)
+    print("PIN code is created")
+
+except ValueError:
+	# and here
+    print("Please enter a number")
