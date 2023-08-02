e629ae587da3585e4b068147fc1d31a3d85762b5
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-02 11:43:38 +0200

    add: python developer: Fitness Goals!

---
+++ courses/python_developer/working_with_data/fitness_goals!.py
@@ -0,0 +1,9 @@
+# Take steps and minutes as inputs
+steps = int(input())
+active_minutes = int(input())
+
+# Store the result of the operations in the variable
+goal_achieved = steps > 10_000 or active_minutes > 30
+
+# Display the result on the screen
+print(goal_achieved)
