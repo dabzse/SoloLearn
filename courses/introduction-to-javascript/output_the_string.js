03fe6eea232f14369c51d07e8b6db248904f7981
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-08-31 13:59:27 +0200

    add: introduction to javascript: Output The String

---
+++ courses/introduction-to-javascript/output_the_string.js
@@ -0,0 +1,10 @@
+// fix the code
+console.log('I\'m learning JavaScript. It\'s easy');
+console.log("I'm learning JavaScript. It's easy");
+
+/**
+ * choose any of these
+ * both are correct
+ * both are acceptable at the moment
+ * but the task prefers the escaped solution
+ */
