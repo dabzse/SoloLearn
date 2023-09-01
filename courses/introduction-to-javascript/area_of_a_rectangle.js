bc8fe5a8cbb427a88fae95ceccee8792186f4ad9
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-01 17:23:42 +0200

    add: introduction to javascript: Area Of a Rectangle

---
+++ courses/introduction-to-javascript/area_of_a_rectangle.js
@@ -0,0 +1,10 @@
+let width = parseInt(readLine(), 10);
+let length = parseInt(readLine(), 10);
+
+// complete the function
+function area(x, y) {
+    return (width * length);
+}
+
+// call the function
+console.log(area());
