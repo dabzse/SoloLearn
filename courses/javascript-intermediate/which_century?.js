59d5f97f986af7776df7a2408bfc897a60496bee
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-02 21:39:50 +0200

    add: javascript intermediate: Which Century?

---
+++ courses/javascript-intermediate/which_century?.js
@@ -0,0 +1,12 @@
+function main() {
+    var year = parseInt(readLine(), 10);
+
+    // the output
+    console.log(calcCent(year));
+
+}
+
+// complete the function
+function calcCent(yr) {
+    return Math.ceil(yr / 100);
+}
