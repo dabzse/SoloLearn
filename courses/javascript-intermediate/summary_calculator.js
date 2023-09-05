39fb49bd43e79db51f663768957cbfac8cccdd9c
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-05 08:02:23 +0200

    add: javascript intermediate: Summary calculator

---
+++ courses/javascript-intermediate/summary_calculator.js
@@ -0,0 +1,12 @@
+// complete the function
+function Add(... input) {
+    let sum = 0;
+    for(let i of input) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(Add(1,2,3));
+console.log(Add(4,14,5,9,14));
+console.log(Add(2,36));
