cd5ef8c6b9204b3aedf5e9db8aa05677358d2722
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-18 19:36:16 +0200

    add: coding foundations: Queue Management

---
+++ courses/coding-foundations/queue_management.py
@@ -0,0 +1,23 @@
+"""
+queue = ['John', 'Amy', 'Bob', 'Adam']
+
+# take an input
+name =
+
+# add the taken value to the end of the queue
+
+
+# display the updated queue
+"""
+
+
+queue = ['John', 'Amy', 'Bob', 'Adam']
+
+# take an input
+name = input()
+
+# add the taken value to the end of the queue
+queue.append(name)
+
+# display the updated queue
+print(queue)
