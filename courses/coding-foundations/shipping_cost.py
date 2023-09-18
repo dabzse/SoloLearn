812fa1d632de2651d5f57b01785872e20b09c3b3
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-18 19:40:42 +0200

    add: coding foundations: Shipping cost

---
+++ courses/coding-foundations/shipping_cost.py
@@ -0,0 +1,21 @@
+"""
+# taking the weight as input
+weight = int(input())
+
+# complete the function
+def shipping_cost():
+
+
+# function call
+shipping_cost(weight)
+"""
+
+# taking the weight as input
+weight = int(input())
+
+# complete the function
+def shipping_cost(w):
+    print(w * 5)
+
+# function call
+shipping_cost(weight)
