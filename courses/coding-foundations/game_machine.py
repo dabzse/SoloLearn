4e65c85366419906ad1619aed20584ca04d9aa14
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-18 19:26:47 +0200

    add: coding foundations: Game Machine

---
+++ courses/coding-foundations/game_machine.py
@@ -0,0 +1,22 @@
+"""
+# installed games
+games = [
+    'Soccer', 'Tic Tac Toe', 'Snake',
+    'Puzzle', 'Rally']
+
+# taking player's choice as a number input
+choice = int(input())
+
+# output the corresponding game
+"""
+
+# installed games
+games = [
+    'Soccer', 'Tic Tac Toe', 'Snake',
+    'Puzzle', 'Rally']
+
+# taking player's choice as a number input
+choice = int(input())
+
+# output the corresponding game
+print(games[choice])
