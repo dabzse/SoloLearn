5d9989076645bdf1259203675a90dfcd87cea536
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-09-18 19:42:34 +0200

    add: coding foundations: Hashtag Generator

---
+++ courses/coding-foundations/hashtag_generator.py
@@ -0,0 +1,20 @@
+"""
+# taking a word as input
+word = input()
+
+def hashtag(word):
+    # complete the function body
+    return
+
+# display the result
+"""
+
+# taking a word as input
+word = input()
+
+def hashtag(w):
+    # complete the function body
+    return '#'+w
+
+# display the result
+print(hashtag(word))
