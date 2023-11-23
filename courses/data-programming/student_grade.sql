050015a10cf98234c00ab3ba12c2874aef633d97
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-11-23 16:08:26 +0100

    add: Data Programming: Student Grade

---
+++ courses/data-programming/student_grade.sql
@@ -0,0 +1,10 @@
+/**
+    select all fields from the students table
+    sort the results based on the score
+    in descending order
+
+    -- pro
+*/
+
+SELECT * FROM students
+ORDER BY score DESC;
