c054ecbfd59ce94b6d9d8cf3d09ed2e4afbb18e6
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-11-23 16:31:23 +0100

    add: Data Programming: Grocery Store

---
+++ courses/data-programming/grocery_store.sql
@@ -0,0 +1,13 @@
+/**
+    table name: products
+    select the name, price, and discount
+    where the discount is greater than 0
+    order by the discount amount in descending order
+
+    -- pro
+*/
+
+SELECT name, price, discount
+FROM products
+WHERE discount > 0
+ORDER BY discount DESC;
