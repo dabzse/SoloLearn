515b8038c03218649c996362992755a1494eee64
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-02 19:46:04 +0100

    add: Introduction to C#: Program Structure

---
+++ courses/csharp-introduction/program_structure.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("C# is awesome!");
+        Console.WriteLine("Coding is fun");
+    }
+}
