b839a7e3da496543bd5dce3f2702c521b1c09ec7
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-04 19:46:36 +0100

    add: Introduction to C#: Name and Score

---
+++ courses/csharp-introduction/name_and_score.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // fix the code
+        string name = "Tom";
+        int score = 163; // it can be byte or short also
+
+        Console.WriteLine(name);
+        Console.WriteLine(score);
+    }
+}
