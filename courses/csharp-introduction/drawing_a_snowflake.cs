41f4e43dbde689b7ac38ddbe1076404764e6a422
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-04 19:40:57 +0100

    add: Introduction to C#: Drawing a Snowflake

---
+++ courses/csharp-introduction/drawing_a_snowflake.cs
@@ -0,0 +1,21 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string p1 = "  *  ";
+        string p2 = " *** ";
+        string p3 = "*****";
+
+
+        // reorder
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p3);
+        Console.WriteLine(p2);
+        Console.WriteLine(p1);
+    }
+}
