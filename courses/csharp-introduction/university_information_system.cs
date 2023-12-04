f6afb893c6c6dfa76ef495c404fe4e821d8b693d
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-04 19:49:01 +0100

    add: Introduction to C#: University Information System

---
+++ courses/csharp-introduction/university_information_system.cs
@@ -0,0 +1,21 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // fix the declarations
+        string name = "John";
+        int age = 21;
+        double score = 4.3;
+        bool isGraduated = false;
+
+        // do not change this code
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(score);
+        Console.WriteLine(isGraduated);
+    }
+}
