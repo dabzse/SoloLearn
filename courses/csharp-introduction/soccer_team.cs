3a96ac69945e108aad31c345caf8fc0b2d1d90ed
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-05 20:45:20 +0100

    add: Introduction to C#: Soccer Team

---
+++ courses/csharp-introduction/soccer_team.cs
@@ -0,0 +1,16 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int wins = 5;
+        int draws = 2;
+
+        // your code goes here
+        Console.WriteLine(wins * 3 + draws);
+        // or Console.WriteLine(wins * 3 + draws * 1);
+    }
+}
