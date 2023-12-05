922623f8fc2324db47bf79f428f293f1ad214a43
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-05 20:40:28 +0100

    add: Introduction to C#: How Many Seconds?

---
+++ courses/csharp-introduction/how_many_seconds?.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        /**
+            30 days
+            1 day = 24 hours
+            1 hour = 60 minutes
+            1 minute = 60 seconds
+        */
+
+        // your code goes here
+        Console.WriteLine(60 * 60 * 24 * 30);
+    }
+}
