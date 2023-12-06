253458713a7390f05ffc350ad42f51add3f214f3
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-06 19:18:54 +0100

    add: Introduction to C#: Surprise!🎁

---
+++ courses/csharp-introduction/surprise!🎁.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int items = 200;
+        int itemInBox = 9;
+
+        // your code goes here
+        Console.WriteLine(items % itemInBox);
+    }
+}
