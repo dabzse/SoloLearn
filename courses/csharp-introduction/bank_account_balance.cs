9ed2e7285634d17b8b03dfe57365ac0f472c4863
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-07 19:37:48 +0100

    add: Introduction to C#: Bank Account Balance

---
+++ courses/csharp-introduction/bank_account_balance.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int balance = 416500;
+        int withdraw = 160000;
+
+        // your code goes here
+        Console.WriteLine(balance - withdraw);
+    }
+}
