cde0a3d9e93e15fa3cccb34a5d2978dfe5773ec1
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-07 19:32:14 +0100

    add: Introduction to C#: Where are my points?

---
+++ courses/csharp-introduction/where_are_my_points?.cs
@@ -0,0 +1,20 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int scoreTom = 5;
+        int scoreBob = 5;
+
+        Console.Write("Tom: ");
+        // fix the code
+        Console.WriteLine(++scoreTom);
+
+        Console.Write("Bob: ");
+        // fix the code
+        Console.WriteLine(--scoreBob);
+    }
+}
