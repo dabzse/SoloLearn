7bd91f826e67d699f42e19997a29ea36082da038
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-09 20:35:35 +0100

    add: Introduction to C#: New Lines

---
+++ courses/csharp-introduction/new_lines.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "ABCD";
+        foreach (var item in text)  {
+            Console.WriteLine(item);
+        }
+    }
+}
