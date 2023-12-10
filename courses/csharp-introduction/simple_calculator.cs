e0dfc32eded11afd57d6f61d5bf330568c33e32f
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-10 20:44:05 +0100

    add: Introduction to C#: Simple Calculator

---
+++ courses/csharp-introduction/simple_calculator.cs
@@ -0,0 +1,15 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        // your code goes here
+        Console.WriteLine($"{number1}+{number2}={number1 + number2}");
+    }
+}
