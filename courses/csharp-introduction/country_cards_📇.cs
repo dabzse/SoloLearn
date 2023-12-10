4e07165560ef69a28729709ece9032959088cf51
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-10 20:37:32 +0100

    add: Introduction to C#: Country Cards 📇

---
+++ courses/csharp-introduction/country_cards_📇.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string country = Console.ReadLine();
+        string capital = Console.ReadLine();
+
+        // your code goes here
+        Console.WriteLine($"Country: {country}, Capital: {capital}");
+    }
+}
