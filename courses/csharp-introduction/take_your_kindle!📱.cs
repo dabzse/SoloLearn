6a5cc6413bdc830e901af8933c8eff58f3cf592f
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-12 20:23:12 +0100

    add: Introduction to C#: Take your kindle!📱

---
+++ courses/csharp-introduction/take_your_kindle!📱.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // your code goes here
+        if (age <= 19) {
+            Console.WriteLine("Take your kindle");
+        }
+    }
+}
