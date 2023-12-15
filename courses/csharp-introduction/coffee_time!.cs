e7238b64188991feb8670ce8b8faa16697f56285
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-15 20:26:53 +0100

    add: Introduction to C#: Coffee Time!

---
+++ courses/csharp-introduction/coffee_time!.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        // your code goes here
+        switch(choice) {
+            case 1:
+                Console.WriteLine("Americano");
+                break;
+            case 2:
+                Console.WriteLine("Espresso");
+                break;
+            case 3:
+                Console.WriteLine("Cappuccino");
+                break;
+            default:
+                Console.WriteLine("Unknown");
+                break;
+        }
+    }
+}
