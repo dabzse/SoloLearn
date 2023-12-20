6af2ae997e8ef344e0e7183de346c3f1e5a7aca8
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-20 21:37:50 +0100

    add: Introduction to C#: Loading...

---
+++ courses/csharp-introduction/loading....cs
@@ -0,0 +1,18 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // call the method
+        Loading();
+    }
+
+    // define the Loading() method
+    static void Loading()
+    {
+        Console.WriteLine("Loading...");
+    }
+}
