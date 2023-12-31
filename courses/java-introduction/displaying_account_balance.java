4762f56ffe3ac74e812eae2542bbc4a683c8f3b0
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-12-31 21:08:21 +0100

    add: Introduction to Java: Displaying Account Balance

---
+++ courses/java-introduction/displaying_account_balance.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        String label = "Balance";
+        double balance = 599.84;
+        /**
+         * you can use float instead of double
+        float balance = 599.84f;
+        */
+
+        System.out.println(label);
+        System.out.println(balance);
+    }
+}
