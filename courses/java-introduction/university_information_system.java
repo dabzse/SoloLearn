1c0e6e68939a78a896fdfee84d10566bf436c98f
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-01 17:32:12 +0100

    add: Introduction to Java: University Information System

---
+++ courses/java-introduction/university_information_system.java
@@ -0,0 +1,19 @@
+// pro
+
+class Demo {
+
+    public static void main(String[] args) {
+
+        // fix variable declarations
+        String name = "John";
+        int age = 21;
+        boolean isGraduated = false;
+        double averageScore = 4.3;
+
+        // do not change this code
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isGraduated);
+        System.out.println(averageScore);
+    }
+}
