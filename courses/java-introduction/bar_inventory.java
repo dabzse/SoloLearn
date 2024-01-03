22e270a488047fec142aa5314ee38791183264b5
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-03 19:18:36 +0100

    add: Introduction to Java: Bar Inventory

---
+++ courses/java-introduction/bar_inventory.java
@@ -0,0 +1,11 @@
+// pro
+
+class Demo
+{
+    public static void main(String[] args) {
+        int juice = 64;
+        int iceTea = 23;
+        // calculate the sum and output it
+        System.out.println(juice + iceTea);
+	}
+}
