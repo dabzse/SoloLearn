9b896d7401856eaa279e18a65cce634da4c5785e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-04 20:22:47 +0100

    add: Introduction to Java: Contact Management System

---
+++ courses/java-introduction/contact_management_system.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        String name = "Howard";
+        int age = 42;
+        System.out.println(name + " is " + age + " years old.");
+    }
+}
