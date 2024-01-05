dfb2e45663b1183c498053cb2f9eb3b49e0984c4
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-05 18:55:37 +0100

    add: Introduction to Java: Working on someone else's code

---
+++ courses/java-introduction/working_on_someone_else's_code.java
@@ -0,0 +1,13 @@
+// pro
+
+class Demo {
+    public static void main(String[] args) {
+        /**
+         * So the program prints some odd
+         * numbers and it works perfectly
+         */
+        System.out.println("1");
+        System.out.println("3");
+        System.out.println("7");
+    }
+}
