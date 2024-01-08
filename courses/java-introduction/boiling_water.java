0372ec273501fbc8c95add9467dccfc49b193143
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-08 20:23:39 +0100

    add: Introduction to Java: Boiling Water

---
+++ courses/java-introduction/boiling_water.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        // your code goes here
+        Scanner boil = new Scanner(System.in);
+        int temp = boil.nextInt();
+        if(temp >= 100) {
+            System.out.println("Boiling");
+        }
+        else{
+            System.out.println("Not boiling");
+        }
+    }
+}
