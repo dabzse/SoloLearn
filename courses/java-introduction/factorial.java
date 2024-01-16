1465a7b2e821f42fde356165b254ec0c21288562
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-16 21:22:47 +0100

    add: Introduction to Java: Factorial

---
+++ courses/java-introduction/factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Demo {
+    public static void main(String[] args) {
+        long fact = 1;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}
