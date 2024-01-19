bbe0b4f5795ed6d1050bff523d4a1c9a7d40f3a8
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-19 21:43:17 +0100

    add: Introduction to Java: Vending Machine

---
+++ courses/java-introduction/vending_machine.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        String[] menu = { "Tea", "Espresso", "Americano", "Water", "Hot Chocolate" };
+        Scanner sc = new Scanner(System.in);
+        // your code goes here
+        int choice = sc.nextInt();
+        if (choice >= 0 && choice < menu.length) {
+            System.out.println(menu[choice]);
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+}
