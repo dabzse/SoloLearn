f51f69784d108691b9845e1f03242f2b8fe63409
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-24 21:24:29 +0100

    add: Introduction to Java: Combine to get "Java"

---
+++ courses/java-introduction/combine_to_get_"java".java
@@ -0,0 +1,12 @@
+// pro
+
+class Demo {
+    public static void main(String[] args) {
+        String[ ][ ] arr = {
+            {"Ja", "Cs", "HT"},
+            {"sharp", "va", "ML"}
+        };
+        // add code below
+        System.out.println(arr[0][0] + arr[1][1]);
+    }
+}
