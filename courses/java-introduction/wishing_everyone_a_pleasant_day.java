d9e52d25d80281d6a2895bfd8f989f6b39a0eb68
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-27 21:15:38 +0100

    add: Introduction to Java: Wishing everyone a pleasant day

---
+++ courses/java-introduction/wishing_everyone_a_pleasant_day.java
@@ -0,0 +1,12 @@
+// pro
+
+class Demo {
+    static void expression() {
+        System.out.println("Have a nice day");
+    }
+
+    public static void main(String[] args) {
+        // call the function below
+        expression();
+    }
+}
