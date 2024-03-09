19609f9eb9773607f8b9a9d93275cd6f3206e0e6
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-03-09 18:09:50 +0100

    add: code coach: fruit bowl.c

---
+++ code-coach/free/easy/fruit bowl.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int fruit;
+    scanf("%d", &fruit);
+
+    // your code goes here
+    int apple = fruit / 2;
+    int pie = apple / 3;
+
+    printf("%d", pie);
+
+    return 0;
+}
