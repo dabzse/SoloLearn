297f2aa2045314d872050dd2c2398912abde140e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-03-16 14:07:31 +0100

    add: Python Developer: Chess Tournament

---
+++ courses/python_developer/working_with_data/chess_tournament.py
@@ -0,0 +1,16 @@
+# pro
+
+# Convert the values into numbers
+wins = int(input())
+ties = int(input())
+
+# 1 win = 3 points
+# 1 tie = 1 point
+# Calculate the score
+score = wins * 3 + ties
+
+# Concatenate the 2 strings to produce a message
+message = "Score: " + str(score)
+
+# Display the message
+print(message)
