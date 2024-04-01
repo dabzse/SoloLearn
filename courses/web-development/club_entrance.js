63f9447b200184193492d18e4c6d8c9dbdaf8089
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-01 13:38:02 +0200

    add: Web Development: Club Entrance

---
+++ courses/web-development/club_entrance.js
@@ -0,0 +1,11 @@
+// pro
+
+let age = parseInt(readLine(), 10);
+
+// your code goes here
+/**
+ *  - "Welcome" if age is greater or equal to 16
+ *  - "Not allowed" if it's not
+ */
+
+console.log(age >= 16 ? "Welcome" : "Not allowed")
