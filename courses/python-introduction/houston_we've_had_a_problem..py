8fb13d20dc54efc4cc9d96f28f29c8b684c23d6b
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-01 17:29:54 +0200

    add: Introduction to Python: Houston, we've had a problem.

---
+++ courses/python-introduction/houston_we've_had_a_problem..py
@@ -0,0 +1,13 @@
+# pro
+
+# Physical test score
+physic_test = int(input())
+
+# Flight simulation test score
+flight_test = int(input())
+
+# Combine the operations to get the final result
+result = physic_test > 90 and flight_test > 85
+
+# Display the result on the screen
+print(result)
