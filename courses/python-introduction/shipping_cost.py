6ac301222185da8806548ac9086669074f3aaaf0
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-01 18:24:08 +0200

    add: Introduction to Python: Shipping cost

---
+++ courses/python-introduction/shipping_cost.py
@@ -0,0 +1,12 @@
+# pro
+
+# taking the weight as input
+weight = int(input())
+
+# complete the function
+def shipping_cost(w):
+    cost = 5
+    print(cost * w)
+
+# function call
+shipping_cost(weight)
