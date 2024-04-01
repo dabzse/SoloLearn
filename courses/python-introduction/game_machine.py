b06226532e574971dd9fd1f3ec81b059afad0341
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-01 18:09:25 +0200

    add: Introduction to Python: Game Machine

---
+++ courses/python-introduction/game_machine.py
@@ -0,0 +1,10 @@
+# pro
+
+# installed games
+games = ["Soccer", "Tic Tac Toe", "Snake", "Puzzle", "Rally"]
+
+# taking player's choice as a number input
+choice = int(input())
+
+# output the corresponding game
+print(games[choice])
