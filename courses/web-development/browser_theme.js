b56fc5fcc597cc9a906004dd4dffe6a11b250861
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-03 15:54:36 +0200

    add: Web Development: Browser Theme

---
+++ courses/web-development/browser_theme.js
@@ -0,0 +1,17 @@
+// pro
+
+let number = parseInt(readLine(), 10);
+
+// your code goes here
+
+switch(number) {
+    case 1:
+        console.log("Light");
+        break;
+    case 2:
+        console.log("Dark");
+        break;
+    case 3:
+        console.log("Indigo");
+        break;
+}
