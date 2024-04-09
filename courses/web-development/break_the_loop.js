e4d94a0a264232bc8c83446c89ab939679feda1e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-09 20:45:28 +0200

    add: Web Development: Break The Loop

---
+++ courses/web-development/break_the_loop.js
@@ -0,0 +1,12 @@
+let num = 1;
+
+for (i = 1; i <= 100; i++) {
+    num *= i;
+    // your code goes here
+    if (num > 10000) {
+        break;
+    }
+}
+
+// generate the result
+console.log(num);
