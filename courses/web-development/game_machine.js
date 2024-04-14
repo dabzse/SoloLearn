371e932aa7ee4a260774dce8ec2f1c1f0d33ac0c
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-14 08:35:56 +0200

    add: Web Development: Game Machine

---
+++ courses/web-development/game_machine.js
@@ -0,0 +1,26 @@
+// pro
+
+let gameNumber = parseInt(readLine(), 10);
+
+/**
+ * 1. Shooter
+ * 2. Puzzle
+ * 3. Snake
+ */
+
+// complete the function
+function run() {
+    switch (gameNumber) {
+        case 1:
+            console.log("Shooter");
+            break;
+        case 2:
+            console.log("Puzzle");
+            break;
+        case 3:
+            console.log("Snake");
+            break;
+    }
+}
+
+run(gameNumber);
