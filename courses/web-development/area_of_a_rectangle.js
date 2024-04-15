98191b8ae707edaf756bbe9f07e46b127c4deb93
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-15 19:14:17 +0200

    add: Web Development: Area Of a Rectangle

---
+++ courses/web-development/area_of_a_rectangle.js
@@ -0,0 +1,10 @@
+let width = parseInt(readLine(), 10);
+let length = parseInt(readLine(), 10);
+
+// Complete the function
+function area(x, y) {
+    return (width * length);
+}
+
+// call the function
+console.log(area());
