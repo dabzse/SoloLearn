560d4655beaceaa149a91a7813cadc6d7278aec3
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-16 08:32:19 +0200

    add: Web Development: Time to Dance!

---
+++ courses/web-development/time_to_dance!.js
@@ -0,0 +1,13 @@
+// pro
+
+let score1 = parseInt(readLine(), 10);
+let score2 = parseInt(readLine(), 10);
+let score3 = parseInt(readLine(), 10);
+
+
+// complete the function
+function average() {
+    return (score1 + score2 + score3) / 3;
+}
+
+console.log(average(score1, score2, score3));
