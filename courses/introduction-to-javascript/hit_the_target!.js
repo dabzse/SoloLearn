65515281ee7d693ce17343a289cf8944d673de36
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-04-22 16:44:29 +0200

    add: Introduction to JavaScript: Hit the Target!

---
+++ courses/introduction-to-javascript/hit_the_target!.js
@@ -0,0 +1,15 @@
+// pro
+
+let points = 0;
+let arrows = 5;
+
+// your code goes here
+
+function shot() {
+    points += 10;
+    arrows--;
+}
+
+shot();
+console.log(points);
+console.log(arrows);
