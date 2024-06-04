235cd8fbad9afa958a0e74bbf63311eb2384d396
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-04 16:12:20 +0200

    add: Front-end for Beginners: Drawing a Snowflake

---
+++ courses/web-developer-with-angular/drawing_a_snowflake.js
@@ -0,0 +1,26 @@
+// pro
+
+/**
+
+let p1 = "  *  ";
+let p2 = " *** ";
+let p3 = "*****";
+
+//reorder
+console.log(p2);
+console.log(p3);
+console.log(p1);
+console.log(p2);
+console.log(p3);
+
+ */
+
+let p1 = "  *  ";
+let p2 = " *** ";
+let p3 = "*****";
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p2);
+console.log(p1);
