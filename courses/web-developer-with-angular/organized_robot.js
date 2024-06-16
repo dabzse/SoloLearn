06ca7f76ea2a56cad49671344daa80ba146b68ed
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-16 09:46:12 +0200

    add: Front-end for Beginners: Organized Robot

---
+++ courses/web-developer-with-angular/organized_robot.js
@@ -0,0 +1,14 @@
+let color = readLine();
+
+/**
+    red => 1
+    green => 2
+    black => 3
+    other cases => unknown
+*/
+
+// your code goes here
+if (color == "red") console.log(1)
+else if (color == "green") console.log(2)
+else if (color == "black") console.log(3)
+else console.log("unknown")
