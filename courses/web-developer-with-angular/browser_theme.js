e4037d1ec0cc2963a2b7efeb56325071334352e8
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-20 19:09:36 +0200

    add: Front-end for Beginners: Browser Theme

---
+++ courses/web-developer-with-angular/browser_theme.js
@@ -0,0 +1,16 @@
+// pro
+
+let number = parseInt(readLine(), 10);
+
+// your code goes here
+switch (number) {
+    case 1:
+        console.log("Light");
+        break;
+    case 2:
+        console.log("Dark");
+        break;
+    case 3:
+        console.log("Indigo");
+        break;
+}
