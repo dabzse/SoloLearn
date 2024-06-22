279a4a5afc426d9c932cfd7ae5cc90dc4633f489
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-22 10:49:32 +0200

    add: Introduction to C#: Getting started: That's the Price

---
+++ courses/csharp-introduction/that's_the_price.cs
@@ -0,0 +1,12 @@
+// pro
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Price:");
+        Console.WriteLine(499);
+    }
+}
