3f236b951bfb526722529518620dc1baad2f4996
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-02 10:31:44 +0200

    add: Coding Foundations: Going Deeper with Python: Messaging App

---
+++ courses/coding-foundations/messaging_app.py
@@ -0,0 +1,10 @@
+# pro
+
+# take the username as input and display it
+name = input()
+print(name)
+
+# take the age as input and display it
+age = input()
+# age = int(input())  ## will work too
+print(age)
