3dc5e84f803b845e80b3342b10429e860c2ee1af
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-02 10:34:58 +0200

    add: Coding Foundations: Going Deeper with Python: Chatbot v1.1

---
+++ courses/coding-foundations/chatbot_v1.1.py
@@ -0,0 +1,11 @@
+# pro
+
+# Take the name as input
+name = input()
+
+# Use concatenation to join 2 strings
+# message = "Nice to meet you, " + name  ## this work well too
+message = f"Nice to meet you, {name}"
+
+# Display the message to the user
+print (message)
