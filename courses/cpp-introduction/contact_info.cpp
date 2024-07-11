2491de10c79d24a8f56ce05ed3b2af598ea55e4f
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-11 17:55:08 +0200

    add: Introduction to C++: Contact Info

---
+++ courses/cpp-introduction/contact_info.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "James";
+    int age = 42;
+    float height = 185.7f;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age;
+    cout << " | Height: " << height;
+}
