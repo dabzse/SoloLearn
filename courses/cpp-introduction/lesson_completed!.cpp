4aaeda3497f6c47480a4bf30de76a4727a0a9298
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-12 10:51:08 +0200

    add: Introduction to C++: Lesson Completed!

---
+++ courses/cpp-introduction/lesson_completed!.cpp
@@ -0,0 +1,13 @@
+// pro
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string lesson = "Data Types";
+    bool isCompleted = true;
+
+    cout << "Lesson: " << lesson << endl;
+    cout << "Completed: " << isCompleted << endl;
+}
