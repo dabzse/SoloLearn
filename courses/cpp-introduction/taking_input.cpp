aa09437bde0763979059cc3d9bfb509e6e2b520d
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-19 08:40:58 +0200

    add: Introduction to C++: Taking Input

---
+++ courses/cpp-introduction/taking_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double length;
+    double height;
+
+    cin >> length;
+    cin >> height;
+
+    double area = length * height;
+    cout << area << endl;
+}
