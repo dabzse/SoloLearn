f94ea078b9e34961cf8ced5058e4891db1b12535
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-20 07:14:39 +0200

    add: Introduction to C++: Country Cards

---
+++ courses/cpp-introduction/country_cards.cpp
@@ -0,0 +1,16 @@
+// pro
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string country, capital;
+
+    // your code goes here
+
+    cin >> country >> capital;
+    cout << "Country: " << country << endl;
+    cout << "Capital: " << capital << endl;
+
+    return 0;
+}
