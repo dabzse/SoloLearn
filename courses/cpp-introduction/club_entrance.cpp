2ae7dbb01af9273e417a725283064387eac5cfc2
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-21 07:16:16 +0200

    add: Introduction to C++: Club Entrance

---
+++ courses/cpp-introduction/club_entrance.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int age;
+    cin >> age;
+    if (age >= 16) {
+        cout << "Welcome";
+    }
+    else {
+        cout << "Not allowed";
+    }
+
+    return 0;
+}
