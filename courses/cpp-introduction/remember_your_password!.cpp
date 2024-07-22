4fa8c5e8e4c4f006cbbf276349137ea3832c9aa4
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-22 08:50:32 +0200

    add: Introduction to C++: Remember your Password!

---
+++ courses/cpp-introduction/remember_your_password!.cpp
@@ -0,0 +1,20 @@
+// pro
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string password, attempt;
+    cin >> password >> attempt;
+
+    // your code goes here
+    if (password == attempt) {
+        cout << "Welcome" << endl;
+    }
+    else {
+        cout << "Try again" << endl;
+    }
+
+    return 0;
+}
