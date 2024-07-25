3d8d7594550fc89ea4f4e019330acc6ad72210cb
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-25 20:58:47 +0200

    add: Introduction to C++: Playground Tickets

---
+++ courses/cpp-introduction/playground_tickets.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if (age >= 0 && age <= 3) {
+        cout << "Free" << endl;
+    }
+    else if (age >= 4 && age <= 6) {
+        cout << "Discounted" << endl;
+    }
+    else {
+        cout << "Normal" << endl;
+    }
+
+    return 0;
+}
