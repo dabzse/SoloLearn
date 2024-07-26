1681c2f53f7dee93b82c87ea1800c491fdf49fdf
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-26 23:39:42 +0200

    add: SQL: The INSERT Statement

---
+++ courses/sql/the_INSERT_statement.sql
@@ -0,0 +1,11 @@
+/**
+    6,   'Mercedes-Benz',   'G 63',   2020
+    7,   'Porsche',     'Panamera',   2020
+*/
+
+INSERT INTO Garage (id, make, model, prodyear)
+VALUES  (6, 'Mercedes-Benz', 'G 63', 2020),
+        (7, 'Porsche', 'Panamera', 2020);
+
+SELECT *
+FROM Garage;
