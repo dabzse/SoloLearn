c91f3e74f2fd120a3ed24d5042ae308ab7434626
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-27 18:09:32 +0200

    add: Introduction to C++: Countdown

---
+++ courses/cpp-introduction/countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    while (number >= 0) {
+        cout << number << endl;
+        number--;
+    }
+
+    return 0;
+}
