962b3a02cbeb2fdde50f4d3b5b456f00df2f0fd2
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-06 15:17:44 +0200

    add: Introduction to C++: Social Media

---
+++ courses/cpp-introduction/social_media.cpp
@@ -0,0 +1,16 @@
+// pro
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string topics[] = {"Sports", "Politics", "Business", "Technology", "Art"};
+
+    // your code goes here
+    int selected_topic;
+    cin >> selected_topic;
+    cout << topics[selected_topic] << endl;
+
+    return 0;
+}
