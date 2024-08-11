2ca1e0c4e0770c80f97e0d74096889dd41f72629
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-11 16:37:28 +0200

    add: Introduction to C++: Game Map

---
+++ courses/cpp-introduction/game_map.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int map[7][7] = {
+        {0, 0, 1, 1, 0, 1, 0},
+        {1, 0, 0, 1, 1, 0, 1},
+        {0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0, 0, 1}
+    };
+
+    int row, column;
+    cin >> row >> column;
+    cout << map[row][column];
+
+    return 0;
+}
