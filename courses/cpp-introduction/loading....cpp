139ced3f92c36679ea8730065cf158af388fb205
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-17 10:57:05 +0200

    add: Introduction to C++: Loading...

---
+++ courses/cpp-introduction/loading....cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// define the loading() function
+void loading() {
+    cout << "Loading...";
+}
+
+int main() {
+    // call the function
+    loading();
+}
