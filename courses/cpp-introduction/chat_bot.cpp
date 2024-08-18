321b03c54bcfc772e4a0c3cb0918d43b15de22c6
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-18 09:18:40 +0200

    add: Introduction to C++: Chat Bot

---
+++ courses/cpp-introduction/chat_bot.cpp
@@ -0,0 +1,16 @@
+// pro
+
+#include <iostream>
+using namespace std;
+
+// define the bot() function here
+
+void bot(string name) {
+    cout << "Welcome, " << name << "!" << endl;
+}
+
+int main() {
+    string name;
+    cin >> name;
+    bot(name);
+}
