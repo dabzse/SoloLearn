342c65203d6cd373a5492b7c4c515cdfa94f2689
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-21 20:21:22 +0200

    add: Introduction to C++: How many seconds?

---
+++ courses/cpp-introduction/how_many_seconds?.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// define the toSeconds() function here
+int toSeconds(int days) {
+    return days * 24 * 60 * 60;
+}
+
+int main() {
+    int days;
+    cin >> days;
+
+    int seconds = toSeconds(days);
+    cout << seconds;
+
+    return 0;
+}
