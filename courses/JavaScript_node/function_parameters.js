55c23407bf1c4ccb275f5e254edc2b5bdb19299f
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-09-16 20:48:23 +0200

    add: JavaScript (node version): Function Parameters

---
+++ courses/JavaScript_node/function_parameters.js
@@ -0,0 +1,10 @@
+function main() {
+    var eventExample = readLine()
+    // function call
+    setReminder(eventExample)
+}
+
+//complete the function
+function setReminder(eventExample) {
+    console.log("You set a reminder about " + eventExample)
+}
