d1ae06f44fc76ec39e40367c22753a9905289493
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-10-09 23:22:16 +0200

    add: JavaScript (node version): Introducing Objects

---
+++ courses/JavaScript_node/introducing_objects.js
@@ -0,0 +1,12 @@
+var cuboid = {
+    length: 25,
+    width: 50,
+    height: 200
+};
+
+// your code goes here
+var l = cuboid.length
+var w = cuboid.width
+var h = cuboid.height
+
+console.log(l * w * h)
