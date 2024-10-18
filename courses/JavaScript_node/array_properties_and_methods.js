e9c93459ca3ca8795840f11e1113cf266e5d5f8f
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-10-18 23:04:10 +0200

    add: JavaScript (node version): Array Properties & Methods

---
+++ courses/JavaScript_node/array_properties_and_methods.js
@@ -0,0 +1,21 @@
+function main() {
+    // take the number of passed levels
+    var levels = parseInt(readLine(), 10);
+    var points = new Array();
+
+    var count = 0;
+    while (count < levels) {
+        var elem = parseInt(readLine(), 10);
+        points[count] = elem;
+        count++;
+    }
+
+    var sum = 0;
+    // calculate the sum of points
+    for (var i = 0; i < levels; i++) {
+        sum += points[i];
+    }
+
+    // output
+    console.log(sum);
+}
