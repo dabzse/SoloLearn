3baf98676647ad2779da7291bbd4713a2f49e5c0
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-11-30 20:47:07 +0100

    add: Front-end for Beginners: Area Of a Rectangle

---
+++ courses/web-developer-with-angular/area_of_a_rectangle.js
@@ -0,0 +1,10 @@
+let width = parseInt(readLine(),10)
+let length = parseInt(readLine(),10)
+
+// complete the function
+function area(x, y) {
+    return x * y
+}
+
+// call the function
+console.log(area(width, length))
