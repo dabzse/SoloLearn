86313bff10ff0aea911666e348f1e48f5ac3d08e
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-02-06 19:38:06 +0100

    add: go: Concurrent Counter

---
+++ courses/go/concurrent_counter.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+// define the count() function
+func count(num int, data []int, ch chan int) {
+    count := 0
+    for _, v := range data {
+        if v == num {
+            count++
+        }
+    }
+    ch <- count
+}
+
+func main() {
+    data := []int{12, 45, 88, 42, 0, 98, 102, 42, 77, 42, 1, 8, 7, 55, 4, 12, 87, 90, 42, 42, 11, 2, 6, 53, 90, 100, 4, 32, 8}
+
+    var num int
+    fmt.Scanln(&num)
+
+    ch1 := make(chan int)
+    ch2 := make(chan int)
+
+    go count(num, data[:len(data)/2], ch1)
+    go count(num, data[len(data)/2:], ch2)
+
+	// output the final result here
+    total := <-ch1 + <-ch2
+
+    fmt.Println(total)
+}
