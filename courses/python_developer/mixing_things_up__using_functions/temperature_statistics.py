2a73efa028c34a6a9026444ec79e29bd2a5f2ae8
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-09-14 20:41:42 +0200

    add: Python Developer: Mixing Things Up: Using Functions: Temperature Statistics

---
+++ courses/python_developer/mixing_things_up__using_functions/temperature_statistics.py
@@ -0,0 +1,11 @@
+temperatures = [22, 25, 19, 21, 24, 28, 23]
+
+# Find the lowest and highest temperature of the week
+min_temp = min(temperatures)
+max_temp = max(temperatures)
+
+# Display the lowest temperature
+print(min_temp)
+
+# Display the highest temperature
+print(max_temp)
